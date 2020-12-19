a4011cc1b30559bb3d0ea376c81ad51fa2936436
Van <van.eliot.santos@gmail.com>
2020-12-19 11:51:22 +0800

    Added status_approved column to User. Added before_validation for status_approved. status_approved shows up on view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_094852) do
+ActiveRecord::Schema.define(version: 2020_12_19_034308) do
 
   create_table "buyers_stocks", force: :cascade do |t|
     t.integer "user_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_094852) do
     t.integer "role_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status_approved"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
