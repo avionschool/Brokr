5cf2d58710be27a847b6c30b73365637f9b7649b
vaneliot <van.eliot.santos@gmail.com>
2021-01-06 17:41:43 +0800

    CSS/UI tweaks; Removed the extra block of data that appears in the stocks page.

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,15 +1,14 @@
 class StockController < ApplicationController
 
     def index
-
         @client = IEX::Api::Client.new(
             publishable_token: 'pk_3fac9a23e6694fdcb684099ec25cf67b',
             secret_token: 'sk_9d27a9ab69c64168ad4fad54cf700ecb',
             endpoint: 'https://cloud.iexapis.com/v1'
           )
 
-        @stocks = @client.stock_market_list(:mostactive)
-        # stocks = @client.ref_data_symbols()
+        @stocks = @client.stock_market_list(:mostactive).slice(0, 10)
+        # @stocks = @client.ref_data_symbols().slice(0, 10)
     end
 
     # def index
