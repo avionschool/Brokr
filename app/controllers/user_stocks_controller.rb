acbf9959cc61e6d28a7717d676b3c3a38deef107
vaneliot <van.eliot.santos@gmail.com>
2021-01-07 00:04:01 +0800

    Can now add stocks to broker page. Still needs warning message for duplicate entries. Still needs other CRUD functionalities.

---
+++ app/controllers/user_stocks_controller.rb
@@ -0,0 +1,61 @@
+class UserStocksController < ApplicationController
+  def index
+    @user_stocks = UserStock.all
+  end
+
+  def show
+    @user_stock = UserStock.find(params[:id])
+  end
+
+  def new
+    # @user_stock = UserStock.new 
+
+    # @stock = Stock.find(params[:stock_id])
+    # @stock.users << current_user
+    # current_user.stocks << @stock
+  end
+
+  def create
+    @user_stock = UserStock.new(user_stock_params)
+
+    @stock = Stock.where(id: params[:stock_id])
+    # @stock.users << current_user
+    current_user.stocks << @stock
+
+    if @user_stock.save
+        # redirect_to user_stocks_path
+        # redirect_to stocks_path
+        redirect_to "/users/#{current_user.id}"
+    # else
+    #     render :new
+    end
+  end
+
+  def edit
+    @user_stock = UserStock.find(params[:id])
+  end
+
+  def update
+    @user_stocks = UserStock.find(params[:id])
+        if @user_stocks.update(user_stock_params)
+            # redirect_to @user_stock
+            # redirect_to user_stocks_path
+            redirect_to stocks_path
+        # else
+            # render :edit
+        end
+  end
+
+  def destroy
+      @user_stock = UserStock.find(params[:id])
+      @user_stock.destroy
+      redirect_to categories_path
+  end
+
+  private
+  def user_stock_params
+      params.require(:user_stock).permit(:user_id, :stock_id, :quantity)
+  end
+
+ 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   # resources :users do
     
   # end
+
+  resources :user_stocks
+
 end
