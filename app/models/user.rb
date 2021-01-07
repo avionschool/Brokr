08b5d99daf2e4ce8aa01928116d202bdc0e4a27c
vaneliot <van.eliot.santos@gmail.com>
2021-01-07 21:00:07 +0800

    Added :confirmable to user.rb. Fixed Devise user paths affected by the new user controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :role_id, presence: true
 
   # kinoment out ko muna tong status approved para makapasok yung broker sa page.
