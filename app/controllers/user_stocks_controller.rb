e6e709cd3d27e0d1988d212d2f4ee26a37a2e509
vaneliot <van.eliot.santos@gmail.com>
2021-01-12 22:58:11 +0800

    Can now see quantity in stock list and user profile; Updating quantity still has problems.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,21 +12,42 @@ class UserStocksController < ApplicationController
   end
 
   def create
-    @user_stock = UserStock.new(user_stock_params)
+    # @user_stock = UserStock.new(user_stock_params)
     # TODO: set user_stock quantity here
+
+    @user_stocks = UserStock.where(stock_id: params[:stock_id], user_id: params[:user_id])
+    if(@user_stocks.count == 0)
+      # create if doesn't yet exist
+      @user_stock = UserStock.new(user_stock_params)
+      @user_stock.save
+    else
+      # update if already exists
+      @user_stock = @user_stocks[0]
+      # @user_stock.update(user_stock_params)
+      # @user_stock.update(:quantity => params[:quantity])
+      @user_stock.quantity = params[:quantity]
+      @user_stock.save
+    end
+
+   
+    # TODO: set user_stock quantity here
+
+    # @user_stock = UserStock.find_or_initialize_by(stock_id: params[:stock_id]).update_attributes(:quantity => params[:quantity])
 
     @stock = Stock.where(id: params[:stock_id])
     # @user = User.where(id: params[:user_id])
     # @stock.users << @user
     current_user.stocks << @stock
 
-    if @user_stock.save
+    
+
+    # if @user_stock.save
         # redirect_to user_stocks_path
         # redirect_to stocks_path
         redirect_to "/users/#{current_user.id}"
     # else
     #     render :new
-    end
+    # end
   end
 
   def edit
@@ -38,7 +59,8 @@ class UserStocksController < ApplicationController
         if @user_stocks.update(user_stock_params)
             # redirect_to @user_stock
             # redirect_to user_stocks_path
-            redirect_to stocks_path
+            # redirect_to stocks_path
+            redirect_to "/users/#{current_user.id}"
         # else
             # render :edit
         end
