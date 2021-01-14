0deff678e1d763e88c73a26a026ff95fb17b9b29
vaneliot <van.eliot.santos@gmail.com>
2021-01-14 19:45:11 +0800

    Can now update user_stock quantity on broker profile.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -13,41 +13,25 @@ class UserStocksController < ApplicationController
 
   def create
     # @user_stock = UserStock.new(user_stock_params)
-    # TODO: set user_stock quantity here
-
-    @user_stocks = UserStock.where(stock_id: params[:stock_id], user_id: params[:user_id])
-    if(@user_stocks.count == 0)
+    @user_stock = UserStock.find_by(stock_id: params[:stock_id], user_id: params[:user_id])
+    if(@user_stock.blank?)
       # create if doesn't yet exist
       @user_stock = UserStock.new(user_stock_params)
-      @user_stock.save
-    else
-      # update if already exists
-      @user_stock = @user_stocks[0]
-      # @user_stock.update(user_stock_params)
-      # @user_stock.update(:quantity => params[:quantity])
-      @user_stock.quantity = params[:quantity]
+
+      @stock = Stock.where(id: params[:stock_id])
+      # @user = User.where(id: params[:user_id])
+      
+      # this @stock.users << @user part has errors
+      # @stock.users << @user
+      current_user.stocks << @stock
+
+      # note: this else part doesn't work
+      # else
+      #   @user_stock.update({:quantity => params[:quantity]})
       @user_stock.save
     end
 
-   
-    # TODO: set user_stock quantity here
-
-    # @user_stock = UserStock.find_or_initialize_by(stock_id: params[:stock_id]).update_attributes(:quantity => params[:quantity])
-
-    @stock = Stock.where(id: params[:stock_id])
-    # @user = User.where(id: params[:user_id])
-    # @stock.users << @user
-    current_user.stocks << @stock
-
-    
-
-    # if @user_stock.save
-        # redirect_to user_stocks_path
-        # redirect_to stocks_path
-        redirect_to "/users/#{current_user.id}"
-    # else
-    #     render :new
-    # end
+    redirect_to "/users/#{current_user.id}"
   end
 
   def edit
@@ -55,15 +39,10 @@ class UserStocksController < ApplicationController
   end
 
   def update
-    @user_stocks = UserStock.find(params[:id])
-        if @user_stocks.update(user_stock_params)
-            # redirect_to @user_stock
-            # redirect_to user_stocks_path
-            # redirect_to stocks_path
-            redirect_to "/users/#{current_user.id}"
-        # else
-            # render :edit
-        end
+    @user_stock = UserStock.find(params[:id])
+    @user_stock.update(user_stock_params)
+    
+    redirect_to "/users/#{current_user.id}"
   end
 
   def destroy
--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -1,6 +1,6 @@
 class UserStock < ApplicationRecord
 
-  validates :stock_id, presence: true, uniqueness: true
+  validates :stock_id, presence: true
 
   belongs_to :user
   belongs_to :stock
